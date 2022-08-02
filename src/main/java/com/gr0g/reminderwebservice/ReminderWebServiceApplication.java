5ac6a70435e6a75857d22ec18ee5a238a2408caf
guctum <greguctum@gmail.com>
2022-08-02 15:51:55 -0400

    init

---
+++ src/main/java/com/gr0g/reminderwebservice/ReminderWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gr0g.reminderwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReminderWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gr0g/reminderwebservice/ReminderWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr0g.reminderwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
