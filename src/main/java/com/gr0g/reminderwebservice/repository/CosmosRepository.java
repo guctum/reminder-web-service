a0a8bb1f7ab5fbb3e799b265c466d6a0c7a05844
Greg Uctum <greguctum@gmail.com>
2022-08-04 22:56:20 -0400

    optimize imports

--- src/main/java/com/gr0g/reminderwebservice/repository/CosmosRepository.java
+++ src/main/java/com/gr0g/reminderwebservice/repository/CosmosRepository.java
@@ -2,17 +2,12 @@ package com.gr0g.reminderwebservice.repository;
 
 import java.util.UUID;
 
-// import java.util.UUID;
-
 import org.springframework.stereotype.Repository;
 
-// import com.gr0g.reminderwebservice.domain.Reminder;
 import com.azure.spring.data.cosmos.repository.ReactiveCosmosRepository;
 import com.gr0g.reminderwebservice.domain.Reminder;
 
 import reactor.core.publisher.Flux;;
-
-// import reactor.core.publisher.Flux;
 
 @Repository
 public interface CosmosRepository extends ReactiveCosmosRepository<Reminder, String> {
